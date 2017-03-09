62529f69325b18fe4755c83fafbbfcc3ff79cf6f
Maxim Samoilov <samoilowmaxim@gmail.com>
2017-03-09 20:38:54 +0300

    hello cycle example

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,24 @@
-import { run } from '@cycle/run'
+import { run, Drivers, FantasyObservable } from '@cycle/run'
+import { makeDOMDriver, div, DOMSource, VNode } from '@cycle/dom'
+import xs, { Stream } from 'xstream'
 
-console.log('run2')
+interface Sources {
+  DOM: DOMSource,
+}
 
+interface Sinks {
+  DOM: Stream<VNode>,
+  [name: string]: FantasyObservable,
+}
+
+function main(sources: Sources): Sinks {
+  return {
+    DOM: xs.of(div('hello cycle')),
+  }
+}
+
+const drivers: Drivers<Sources, Sinks> = {
+  DOM: makeDOMDriver('#app')
+}
+
+run(main, drivers)
